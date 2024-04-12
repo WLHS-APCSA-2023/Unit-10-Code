f830e6c85468a18e8d0140a7750dfb585a6d06c9
smithbwlhs <smithb@wlwv.k12.or.us>
2024-04-12 13:53:16 -0700

    initial commit

---
+++ Sorting/src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int[] numbers = {4,5,1,2,3};
+    }
+
+    public static int[] selectionSort(int[] arr){
+
+    
+    }
+}
